b1bd468e68f633add070054689f0b54febf76fd2
Ashley Kilgour <kilgourkilgour@hotmail.com>
2021-07-23 14:53:59 +0100

    Added the Distillery model Also added my common base object

---
+++ WhiskyCrate.Domain/BaseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WhiskyCrate.Domain
+{
+    public abstract class BaseModel
+    {
+        [Key]
+        [Required]
+        [Column(Order = 0)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
---
+++ WhiskyCrate.Domain/Distillery/Distillery.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhiskyCrate.Domain.Distillery
+{
+    public class Distillery : BaseModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public bool CurrentlyOperating { get; set; }
+        public string Region { get; set; }
+    }
+}
